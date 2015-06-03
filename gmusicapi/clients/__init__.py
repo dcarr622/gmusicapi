0bc0667e8d494755f44639657c6161bb8b5f6be6
David Carr <dcarr@scienceprousa.com>
2015-06-03 01:21:55 -0700

    remove filepath

--- gmusicapi/clients/__init__.py
+++ gmusicapi/clients/__init__.py
@@ -1,4 +1,4 @@
 from gmusicapi.clients.webclient import Webclient
 from gmusicapi.clients.mobileclient import Mobileclient
 
-(Webclient, Mobileclient, OAUTH_FILEPATH)  # noqa
+(Webclient, Mobileclient)  # noqa
