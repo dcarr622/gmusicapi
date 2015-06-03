c08240317f9d5dadd342819e8b431500f3c6f82a
David Carr <dcarr@scienceprousa.com>
2015-06-03 01:19:57 -0700

    remove musicmanager

--- gmusicapi/__init__.py
+++ gmusicapi/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from gmusicapi._version import __version__
-from gmusicapi.clients import Webclient, Musicmanager, Mobileclient
+from gmusicapi.clients import Webclient, Mobileclient
 from gmusicapi.exceptions import CallFailure
 
 __copyright__ = 'Copyright 2014 Simon Weber'
@@ -9,7 +9,7 @@ __license__ = 'BSD 3-Clause'
 __title__ = 'gmusicapi'
 
 # appease flake8: the imports are purposeful
-(__version__, Webclient, Musicmanager, Mobileclient, CallFailure)
+(__version__, Webclient, Mobileclient, CallFailure)
 
 
 class Api(object):
